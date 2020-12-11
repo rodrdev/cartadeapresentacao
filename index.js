c046332c6d811f98a3eb9a212c9cff1bba6fd243
Rodrigo Ribeiro <rodrigo.dev@outlook.pt>
2020-12-11 15:58:20 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("test");
+
+function gerar() {
+  /* var img = document.querySelector("#img").value;
+  if (img) {
+    document.querySelector("#circle").src = img;
+  } else {
+    alert("Imagem inválida!");
+  }*/
+  var nome = document.querySelector("#nome").value;
+  var sobrenome = document.querySelector("#sobrenome").value;
+  var cargo = document.querySelector("#cargo").value;
+  var empresa = document.querySelector("#empresa").value;
+}
