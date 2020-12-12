4de16563877ab1fb2d1da9caa26a85849be62e09
Rodrigo Ribeiro <rodrigo.dev@outlook.pt>
2020-12-12 18:37:56 -0300

    Desafio concluido

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 console.log("test");
 
 function gerar() {
-  /* var img = document.querySelector("#img").value;
-  if (img) {
-    document.querySelector("#circle").src = img;
-  } else {
-    alert("Imagem inválida!");
-  }*/
   var nome = document.querySelector("#nome").value;
   var sobrenome = document.querySelector("#sobrenome").value;
   var cargo = document.querySelector("#cargo").value;
   var empresa = document.querySelector("#empresa").value;
+  var img = document.querySelector("#img").value;
+
+  if (img && nome && sobrenome && cargo && empresa) {
+    document.querySelector("#circle").src = img;
+    document.querySelector("#localnome").textContent = `${nome} ${sobrenome}`;
+    document.querySelector("#localcargo").textContent = cargo;
+    document.querySelector("#localempresa").textContent = empresa;
+    mostra();
+    ocultar();
+  } else {
+    alert("Todos os campos devem ser preenchidos");
+  }
+}
+
+function ocultar() {
+  document.querySelector(".test").style.display = "none";
+}
+
+function mostra() {
+  document.querySelector("#informacoes").style.display = "block";
 }
